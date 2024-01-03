730efb65bbb1d61a509fdc90ed912a266bf535ce
TipTopClone <sujanmahat2468@gmail.com>
2024-01-03 22:39:07 +1100

    first commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,80 @@
+import config from '../conf/conf';
+
+import { Client, Account, ID } from 'appwrite';
+
+// const client = new Client()
+//   .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+//   .setProject('<PROJECT_ID>'); // Your project ID
+
+// const account = new Account(client);
+
+// const promise = account.create('[USER_ID]', 'email@example.com', '');
+
+// promise.then(
+//   function (response) {
+//     console.log(response); // Success
+//   },
+//   function (error) {
+//     console.log(error); // Failure
+//   }
+// );
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        //call another method
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      throw error;
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
